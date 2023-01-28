508ed29dde424d543ed2987b0670ebc5cbdaa1d2
dimensionalchaos <90750149+dimensionalchaos@users.noreply.github.com>
2023-01-28 15:35:14 +0530

    sbeve

--- practice/Assets/raja/scripts/Player1move.cs
+++ practice/Assets/raja/scripts/Player1move.cs
@@ -8,8 +8,18 @@ public class Player1move : MonoBehaviour
 [SerializeField]
 private float dirx;
 private float speed = 10f;
+private Rigidbody2D mybody;
+private SpriteRenderer sr;
+private Animator anim;
+
+private void Awake() {
+ mybody=GetComponent<Rigidbody2D>();
+ anim = GetComponent<Animator>();
+ sr = GetComponent<SpriteRenderer>();
 
 
+    
+}
     // Start is called before the first frame update
     void Start()
     {
@@ -19,14 +29,23 @@ private float speed = 10f;
     // Update is called once per frame
     void Update()
     {
+       
         PlayerMovement();
+         PlayerAnimation();
     }
     void PlayerMovement()
     {
         dirx = Input.GetAxisRaw("Horizontal");
         transform.position+= new Vector3(dirx,0,0)*speed* Time.deltaTime;
-
-
         
+    }
+    void PlayerAnimation()
+    {
+        if(dirx!=0)
+        anim.SetBool("walk",true);
+        else 
+        anim.SetBool("walk",false);
+        
+      
     }
 }
